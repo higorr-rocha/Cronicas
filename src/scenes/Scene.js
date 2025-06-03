0065930dc598db6f46f36eea61cc6571e31810c3
Felipe Matrone <162156046+FelipeMatrone@users.noreply.github.com>
2025-06-03 20:37:34 -0300

    Adição de colisão e zoom do mapa (#5)

--- src/scenes/Scene.js
+++ src/scenes/Scene.js
@@ -6,14 +6,32 @@ export class Scene extends Phaser.Scene {
     }
 
     create() {
-        // Adiciona o fundo centralizado
-        this.add.image(640, 360, 'background');
+        const map = this.make.tilemap({ key: 'templo' });
+        const tileset = map.addTilesetImage('templo', 'tiles');
 
-        // Adiciona a personagem na cena
-        const centerX = this.cameras.main.width / 2;
-        const centerY = this.cameras.main.height / 2;
+        map.createLayer('chao', tileset, 0, 0);
+        const paredes = map.createLayer('parede', tileset, 0, 0);
+        paredes.setCollisionByProperty({ colisao: true });
+        const porta = map.createLayer('porta fechada', tileset, 0, 0);
+        porta.setCollisionByProperty({ colisao: true });
+        map.createLayer('botao', tileset, 0, 0);
 
-        this.lyra = new Lyra(this, centerX - 428, centerY + 125);
+        const centerMapX = map.widthInPixels / 2;
+        const centerMapY = map.heightInPixels / 2;
+
+        this.lyra = new Lyra(this, centerMapX -600, centerMapY-20);
+
+        this.physics.add.collider(this.lyra, paredes);
+        this.physics.add.collider(this.lyra, porta);
+
+        // Faz a câmera seguir Lyra
+        this.cameras.main.startFollow(this.lyra);
+
+        // Define os limites da câmera para o tamanho do mapa
+        this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+
+        // Aplica zoom (opcional)
+        this.cameras.main.setZoom(2);
     }
 
     update() {
